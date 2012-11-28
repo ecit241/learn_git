3559425521ea64076793776fecfac161a71523c8
Guo Shaojun <shaojun.guo@kindroid.com>
2012-11-28 16:00:31 +0800

    add int_size.c
    Signed-off-by: Guo Shaojun <shaojun.guo@kindroid.com>

---
+++ int_size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void
+print_size(const char *type, long unsigned size)
+{
+	char *bytes = "Bytes";
+	if (size <= 1) {
+		bytes = "Byte";
+	}
+	printf("Size of %s: %lu %s\n", type, size, bytes);
+}
+
+void main()
+{
+	print_size("int", sizeof(int));
+	print_size("char", sizeof(char));
+	print_size("short int", sizeof(short));
+	print_size("long int", sizeof(long));
+}
